d70d6a8b1690102f3f2fa3b3f1ad9d91755d9678
Tri Kiet <trikiet100@gmail.com>
2021-01-18 13:26:32 +0700

    Remove Redux devtools code

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -3,8 +3,8 @@ import thunk from "redux-thunk";
 import rootReducer from "../reducers/rootReducer";
 
 const enhancer = compose(
-  applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(thunk)
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 const store = createStore(rootReducer, enhancer);
